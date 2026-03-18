da414d19d1f78b9e01f94b30fdb9892728cea03d
Oleki <olekignaczak78@gmail.com>
2026-03-18 01:02:47 +0100

    deleted depracted match history dependencies

--- src/main/java/com/example/demo/hibernate/HibernateConfig.java
+++ src/main/java/com/example/demo/hibernate/HibernateConfig.java
@@ -1,5 +1,4 @@
 package com.example.demo.hibernate;
-import com.example.demo.model.MatchHistory;
 import com.example.demo.model.User;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.hibernate.SessionFactory;
@@ -30,7 +29,6 @@ public class HibernateConfig {
                 configuration.setProperties(settings);
 
                 configuration.addAnnotatedClass(User.class);
-                configuration.addAnnotatedClass(MatchHistory.class);
 //                configuration.addAnnotatedClass(Friends.class);
 
                 sessionFactory = configuration.buildSessionFactory();
