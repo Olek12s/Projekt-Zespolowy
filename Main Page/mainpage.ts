402a7e21be5a948f1eef005277509ae2d4c1f711
JanekGoorniak <161734284+JanekGoorniak@users.noreply.github.com>
2026-03-25 11:44:16 +0100

    ui files upload
    uploaded files for login page and main page

---
+++ Main Page/mainpage.ts
@@ -0,0 +1,6 @@
+function playButtonClicked(){alert("i'd like to play a game...")}
+function spectateButtonClicked(){alert("so you prefer to watch...")}
+function rankingButtonClicked(){alert("something something casual competetive implications etc.")}
+function historyButtonClicked(){alert("checking search history, stand by...")}
+function myAccountButtonClicked(){alert("their mine, natchos!");}
+function friendsButtonClicked(){alert("you don't have any :(")}
