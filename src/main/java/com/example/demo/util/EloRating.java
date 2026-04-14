fa126015b33e2c0b45f314262ceeeb36d17e97a8
Oleki <olekignaczak78@gmail.com>
2026-04-15 01:28:44 +0200

    fix

--- src/main/java/com/example/demo/util/EloRating.java
+++ src/main/java/com/example/demo/util/EloRating.java
@@ -9,7 +9,13 @@ public class EloRating
         return 1 / (1 + Math.pow(10, (ratingWhite-ratingBlack)/400));
     }
 
-
+    /**
+     *
+     * @param ratingWhite White player rating
+     * @param ratingBlack Black player rating
+     * @param matchOutcome who won match: 1 for white, 0 for black, 0.5 for DRAW
+     * @return Pair representing new updated ratings as: &lt; white, black &gt;
+     */
     public static Pair<Double, Double> getOutcome(double ratingWhite, double ratingBlack, MatchOutcome matchOutcome) {
         // calc winning probabilities
         double expectedWhite = expectedScore(ratingBlack, ratingWhite);
@@ -24,6 +30,27 @@ public class EloRating
 
     // tests
     public static void main(String[] args) {
+        Pair<Double, Double> outcome1 = getOutcome(1200, 1000, MatchOutcome.WHITE_WIN);
+        Pair<Double, Double> outcome2 = getOutcome(1200, 1000, MatchOutcome.BLACK_WIN);
+        Pair<Double, Double> outcome3 = getOutcome(1200, 1000, MatchOutcome.DRAW);
+        System.out.printf("1) White: %.2f Black: %.2f%n", outcome1.first(), outcome1.second());
+        System.out.printf("2) White: %.2f Black: %.2f%n", outcome2.first(), outcome2.second());
+        System.out.printf("3) White: %.2f Black: %.2f%n", outcome3.first(), outcome3.second());
 
+        Pair<Double, Double> outcome4 = getOutcome(1000, 1000, MatchOutcome.WHITE_WIN);
+        Pair<Double, Double> outcome5 = getOutcome(1000, 1000, MatchOutcome.BLACK_WIN);
+        Pair<Double, Double> outcome6 = getOutcome(1000, 1000, MatchOutcome.DRAW);
+        System.out.printf("4) White: %.2f Black: %.2f%n", outcome4.first(), outcome4.second());
+        System.out.printf("5) White: %.2f Black: %.2f%n", outcome5.first(), outcome5.second());
+        System.out.printf("6) White: %.2f Black: %.2f%n", outcome6.first(), outcome6.second());
+
+        Pair<Double, Double> outcome7 = getOutcome(600, 1500, MatchOutcome.WHITE_WIN);
+        Pair<Double, Double> outcome8 = getOutcome(200, 1500, MatchOutcome.BLACK_WIN);
+        Pair<Double, Double> outcome9 = getOutcome(2800, 2750, MatchOutcome.DRAW);
+        Pair<Double, Double> outcome10 = getOutcome(1250, 1200, MatchOutcome.DRAW);
+        System.out.printf("7) White: %.2f Black: %.2f%n", outcome7.first(), outcome7.second());
+        System.out.printf("8) White: %.2f Black: %.2f%n", outcome8.first(), outcome8.second());
+        System.out.printf("9) White: %.2f Black: %.2f%n", outcome9.first(), outcome9.second());
+        System.out.printf("10) White: %.2f Black: %.2f%n", outcome10.first(), outcome10.second());
     }
 }
