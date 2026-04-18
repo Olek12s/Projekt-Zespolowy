5a20b73cb80f0ad136b94fd5792c7886edfc5f13
Oleki <olekignaczak78@gmail.com>
2026-04-18 15:04:46 +0200

    assertions Junit5 tests

--- src/test/java/com/example/demo/util/validator/standardMoves/MoveValidatorPawnTest.java
+++ src/test/java/com/example/demo/util/validator/standardMoves/MoveValidatorPawnTest.java
@@ -8,16 +8,120 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.Assert.assertFalse;
 
 public class MoveValidatorPawnTest extends MoveValidatorTester {
+
+    // ========== FROM STARTING LAYOUT ==========
+
     @Test
-    public void shouldAllowPawnMoveE2E4FromStart() {
-        assertTrue(legal("e2e4", true));
+    public void shouldAllowPawnMoveTwoSquaresFromStart() {
+        assertTrue("e2e4 should be legal", legal("e2e4", true));  // white
+        assertFalse("e2e4 should be illlegal", legal("e2e4", false));  // black
     }
 
     @Test
-    public void shouldNotAllowPawnMoveE2E7FromStart() {
-        assertFalse(legal("e2e7", true));
+    public void shouldNotAllowPawnMoveTwoSquaresFromStart() {
+        //assertFalse(legal("e2e7", true));
+    }
+
+    @Test
+    public void shouldAllowPawnMoveOneSquareFromStart() {
+
+    }
+
+    @Test
+    public void shouldNotAllowPawnMoveOneSquareFromStart() {
+
+    }
+
+    @Test
+    public void shouldAllowPawnMoveOneDiagonalSquareFromStart() {
+
+    }
+
+    @Test
+    public void shouldAllowPawnMoveTwoDiagonalSquaresFromStart() {
+
+    }
+
+
+    // ========== FROM PAWN ON MIDDLE LAYOUT, NOT BLOCKED AT ANY TEST ==========
+
+    @Test
+    public void shouldAllowPawnMoveTwoSquaresFromMiddle() {
+        //assertTrue(legal("e2e4", true));  // white
+        //assertTrue(legal("e2e4", false));  // black
+    }
+
+    @Test
+    public void shouldNotAllowPawnMoveTwoSquaresFromMiddle() {
+        //assertFalse(legal("e2e7", true));
+    }
+
+    @Test
+    public void shouldAllowPawnMoveOneSquareFromMiddle() {
+
+    }
+
+    @Test
+    public void shouldNotAllowPawnMoveOneSquareFromMiddle() {
+
+    }
+
+    @Test
+    public void shouldAllowPawnMoveOneDiagonalSquareFromMiddle() {
+
+    }
+
+    @Test
+    public void shouldAllowPawnMoveTwoDiagonalSquaresFromMiddle() {
+
+    }
+
+
+    // ========== FROM PAWN ON MIDDLE LAYOUT, BLOCKED AT EVERY TEST ==========
+
+    @Test
+    public void shouldAllowPawnMoveTwoSquaresFromMiddleBlocked() {
+        //assertTrue(legal("e2e4", true));  // white
+        //assertTrue(legal("e2e4", false));  // black
+    }
+
+    @Test
+    public void shouldNotAllowPawnMoveTwoSquaresFromMiddleBlocked() {
+        //assertFalse(legal("e2e7", true));
+    }
+
+    @Test
+    public void shouldAllowPawnMoveOneSquareFromMiddleBlocked() {
+
+    }
+
+    @Test
+    public void shouldNotAllowPawnMoveOneSquareFromMiddleBlocked() {
+
+    }
+
+    @Test
+    public void shouldAllowPawnMoveOneDiagonalSquareFromMiddleBlocked() {
+
+    }
+
+    @Test
+    public void shouldAllowPawnMoveTwoDiagonalSquaresFromMiddleBlocked() {
+
+    }
+
+    // ========== FROM PAWN TO OTHER PAWN SAME COLOR ON STARTING LAYOUT, BLOCKED AT EVERY TEST ==========
+
+    @Test
+    public void shouldAllowPawnMoveToLeftOntoOtherPawnFromStart() {
+
+    }
+
+    @Test
+    public void shouldAllowPawnMoveToRightOntoOtherPawnFromStart() {
+
     }
 }
