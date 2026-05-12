f2c3af3bdc8443c54358bd712cc2c18862cd56db
JanekGoorniak <Jangoorniak@int.pl>
2026-05-12 21:29:32 +0200

    separated js for main page

---
+++ src/main/resources/static/mainpage.js
@@ -0,0 +1,49 @@
+let client = null;
+
+function connectWebSocket() 
+{
+    const token = localStorage.getItem("token");
+
+    const socket = new SockJS(`http://localhost:8089/ws?token=${token}`);
+    client = Stomp.over(socket);
+
+    client.connect({}, () => 
+    {
+        console.log("Połączono z WebSocket");
+
+        client.subscribe("/user/queue/game", (msg) => 
+        {
+            const data = JSON.parse(msg.body);
+            console.log("MATCH RESPONSE:", data);
+
+            if (data.color === "WAITING") 
+            {
+                alert("Szukamy przeciwnika...");
+            } 
+            else 
+            {
+                alert("Znaleziono gre! ID: " + data.gameId + " kolor: " + data.color);
+
+                localStorage.setItem("gameId", data.gameId);
+                window.location = "game.html";
+            }
+        });
+    });
+}
+
+function playButtonClicked() 
+{
+    if (!client) 
+    {
+        connectWebSocket();
+
+        setTimeout(() => 
+        {
+            client.send("/app/join", {}, {});
+        }, 500);
+    } 
+    else 
+    {
+        client.send("/app/join", {}, {});
+    }
+}
